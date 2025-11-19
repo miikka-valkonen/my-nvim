36dd8d66a71137bf56d5619b280253792e152724
Miikka Valkonen <miikka.valkonen@if.fi>
2025-11-19 10:05:45 +0200

    Added dap ui

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,8 +1,26 @@
 return {
   {
     'mfussenegger/nvim-dap',
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+      'nvim-neotest/nvim-nio',
+    },
     config = function()
       local dap = require 'dap'
+      local dapui = require 'dapui'
+
+      dapui.setup()
+
+      -- auto-open/close UI
+      dap.listeners.after.event_initialized['dapui_config'] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated['dapui_config'] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited['dapui_config'] = function()
+        dapui.close()
+      end
 
       -- Keymaps for controlling the debugger
       vim.keymap.set('n', '<leader>dq', function()
