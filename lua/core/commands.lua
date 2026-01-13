3dc47bfa898fdb41e89c6c8edbc5afb7505b28d1
Miikka Valkonen <miikka.valkonen@if.fi>
2026-01-13 10:57:10 +0200

    Close codecompanion window on persistence save

--- lua/core/commands.lua
+++ lua/core/commands.lua
@@ -20,7 +20,7 @@ vim.api.nvim_create_autocmd('User', {
     for _, win in ipairs(vim.api.nvim_list_wins()) do
       local buf = vim.api.nvim_win_get_buf(win)
       local ft = vim.api.nvim_get_option_value('filetype', { buf = buf })
-      if ft == 'dbui' or ft == 'dbout' then
+      if ft == 'dbui' or ft == 'dbout' or ft == 'codecompanion' then
         vim.api.nvim_win_close(win, true)
       end
     end
