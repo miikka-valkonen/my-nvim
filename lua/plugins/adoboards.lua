21e47dea765ad9849d715ad1dc72a23847c3daf1
Miikka Valkonen <miikka.valkonen@if.fi>
2026-03-03 17:19:38 +0200

    Adoboards command keymap for current WI

--- lua/plugins/adoboards.lua
+++ lua/plugins/adoboards.lua
@@ -3,5 +3,16 @@ return {
   opts = {},
   keys = {
     { '<leader>wb', '<cmd>Adoboards<cr>', desc = 'Ado[B]oards' },
+    {
+      '<leader>wB',
+      function()
+        local workItem = vim.fn.system('print_branch.sh'):gsub('%s+$', '')
+        if not workItem:match '^%d+$' then
+          return
+        end
+        vim.cmd(string.format('Adoboards %s', workItem))
+      end,
+      desc = 'Ado[B]oards for current WI',
+    },
   },
 }
