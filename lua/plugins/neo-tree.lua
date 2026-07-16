516aabedf967ba2d7ea910c6634278989bbeac80
miigha <tech@valkonen.cc>
2026-07-16 12:19:19 +0300

    Close neo-tree on file open

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -85,5 +85,13 @@ return {
         end,
       },
     },
+    event_handlers = {
+      {
+        event = "file_open_requested",
+        handler = function()
+          require("neo-tree.command").execute({ action = "close" })
+        end
+      },
+    },
   },
 }
